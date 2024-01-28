135b94fe28573a38848c55761f9575dd98ba685d
SoJuSo <asdfg7123@gmail.com>
2024-01-29 01:23:22 +0900

    ✨feat : 선택지 추가
    커맨드 명령어에 도시 선택지 추가

---
+++ register-command.js
@@ -0,0 +1,105 @@
+import "dotenv/config";
+import { ApplicationCommandOptionType, REST, Routes } from "discord.js";
+
+const { TOKEN } = process.env;
+
+const commands = [
+  {
+    name: "날씨",
+    description: "위치에 따라 날씨를 알려드려요.",
+    options: [
+      {
+        name: "도시",
+        description: "도시를 선택하세요.",
+        type: ApplicationCommandOptionType.String,
+        required: true,
+        choices: [
+          {
+            name: "서울특별시",
+            value: "서울특별시",
+          },
+          {
+            name: "부산광역시",
+            value: "부산광역시",
+          },
+          {
+            name: "대구광역시",
+            value: "대구광역시",
+          },
+          {
+            name: "인천광역시",
+            value: "인천광역시",
+          },
+          {
+            name: "광주광역시",
+            value: "광주광역시",
+          },
+          {
+            name: "대전광역시",
+            value: "대전광역시",
+          },
+          {
+            name: "울산광역시",
+            value: "울산광역시",
+          },
+          {
+            name: "세종특별자치시",
+            value: "세종특별자치시",
+          },
+          {
+            name: "경기도",
+            value: "경기도",
+          },
+          {
+            name: "강원도",
+            value: "강원도",
+          },
+          {
+            name: "충청북도",
+            value: "충청북도",
+          },
+          {
+            name: "충청남도",
+            value: "충청남도",
+          },
+          {
+            name: "전라북도",
+            value: "전라북도",
+          },
+          {
+            name: "전라남도",
+            value: "전라남도",
+          },
+          {
+            name: "경상북도",
+            value: "경상북도",
+          },
+          {
+            name: "경상남도",
+            value: "경상남도",
+          },
+          {
+            name: "제주특별자치도",
+            value: "제주특별자치도",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const rest = new REST().setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log("슬래시 명령어를 등록하고 있습니다.");
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+      body: commands,
+    });
+
+    console.log("슬래시 명령어 등록이 완료되었습니다.");
+  } catch (error) {
+    console.error(error);
+  }
+})();

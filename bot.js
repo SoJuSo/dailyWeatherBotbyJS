3fe01be63de19318a4ea6d22fa81d6a34cb9d272
SoJuSo <asdfg7123@gmail.com>
2024-01-29 11:29:48 +0900

    💡comment : bot.js 주석 삭제

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ import "dotenv/config";
 import { Client, EmbedBuilder, GatewayIntentBits } from "discord.js";
 import { getWeather } from "./api.js";
 
-// const prefix = "!";
-
 const { TOKEN } = process.env;
 
 const client = new Client({
@@ -18,41 +16,14 @@ client.on("ready", () => {
   console.log(`${client.user.tag} has logged In!`);
 });
 
-// client.on("messageCreate", async (msg) => {
-//   if (msg.author.bot) return;
-//   console.log(msg);
-//   // console.log(msg.content);
-//   // console.log(msg.createdAt.toDateString());
-//   // console.log(msg.author.tag);
-//   msg.reply(`메아리입니다. ${msg.content}`);
-//   // await interaction.reply("메세지!");
-// });
-
-// module.exports = {
-//   data: new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!"),
-//   async execute(interaction) {
-//     await interaction.reply("Pong!");
-//   },
-// };
-
 client.on("interactionCreate", async (interaction) => {
-  // console.log(interaction);
-
   if (!interaction.isChatInputCommand()) return;
 
-  // if (interaction.commandName === "ping") {
-  //   await interaction.reply("Pong!");
-  // }
-
-  // if (interaction.commandName === "헬로") {
-  //   await interaction.reply("하이!");
-  // }
   if (interaction.commandName === "날씨") {
     console.log(interaction.createdAt);
     const city = interaction.options.get("도시").value;
     console.log(city);
 
-    // await interaction.reply(`${city}의 날씨 안내해드리겠습니다!`);
     const data = await getWeather(city);
     const temperature = await data
       .map((val) => {
@@ -65,17 +36,5 @@ client.on("interactionCreate", async (interaction) => {
     await interaction.reply({ embeds: [embed] });
   }
 });
-
-// client.on("messageCreate", (msg) => {
-//   if (msg.author.bot) return;
-//   if (!msg.content.startsWith(prefix)) return;
-//   if (msg.content.slice(0, prefix.length) !== prefix) return;
-
-//   const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-//   const command = args.shift().toLowerCase();
-
-//   const cmd = client.commands.get(command);
-//   if (cmd) cmd.run(client, msg, args);
-// });
 
 client.login(TOKEN);

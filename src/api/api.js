ccabb4216ac8a8cd6e82073286d762efb1d38fe1
SoJuSo <asdfg7123@gmail.com>
2024-02-04 01:46:00 +0900

    🐛fix : 날짜 관련 고민사항 수정
    기존의 날짜는 임의로 이전의 시간을 가져오도록 구성하였음.
    다만 이럴 경우 연도가 넘어가는 경우, 월, 일이 넘어가는 경우 따로 다 처리했어야함.
    
    따라서 로직이 매우 복잡해졌고 차라리 UTC데이터에서 KST - 1 의 시간에 해당하는 함수를 만들어주는 것이
    더 편하다고 판단, 후 적용

--- src/api/api.js
+++ src/api/api.js
@@ -4,21 +4,24 @@ import { getCurrentKRTime, getTargetTimeZone } from "./api.util.js";
 const { BASE_URL, SERVICE_KEY } = process.env;
 
 export const getCurrentWeather = async (city) => {
-  const time = getCurrentKRTime();
-  const [targetDate, targetTime] = getTargetTimeZone(time);
-
   try {
+    const time = getCurrentKRTime();
+    const [targetDate, targetTime] = getTargetTimeZone(time);
     const cord = filterlist[city];
+
     const res = await fetch(
       `${BASE_URL}?serviceKey=${SERVICE_KEY}&pageNo=1&numOfRows=10&dataType=JSON&base_date=${targetDate}&base_time=${
         Number(targetTime) > -1 ? targetTime : 23
       }00&nx=${cord.x}&ny=${cord.y}`
     );
+    // console.log(res);
+    console.log(targetDate, Number(targetTime) > -1 ? targetTime : 23, cord.x, cord.y);
     const data = await res.json();
     const items = await data.response.body.items.item;
-    // console.log(items);
+    console.log(items);
     return { items, time: targetTime };
   } catch (error) {
+    console.log(error);
     return { items: null, time: null };
   }
 };
--- src/api/api.util.js
+++ src/api/api.util.js
@@ -13,22 +13,46 @@ export const getCurrentKRTime = () => {
   return kr_curr;
 };
 
-export const getTargetTimeZone = (date) => {
-  const time = new Date(date);
+export const getCurrentMinusTime = () => {
+  // 1. 현재 시간(Locale)
+  const curr = new Date();
+  console.log(curr);
+  // 2. UTC 시간 계산
+  const utc = curr.getTime() + curr.getTimezoneOffset() * 60 * 1000;
+  // 3. UTC to KST (UTC + 8시간)
+  const KR_TIME_DIFF = 8 * 60 * 60 * 1000;
+  console.log(utc);
+  const kr_curr = new Date(utc + KR_TIME_DIFF);
+  // 4. KST - 1 반환
+  console.log("한국 현재 시간 - 1", kr_curr);
+  return kr_curr;
+};
 
-  const targetTime = String(
-    time.getMinutes() > 40 ? time.getHours() : time.getHours() - 1
-  ).padStart(2, "0");
+export const getTargetTimeZone = (date) => {
+  const tempTime = new Date(getCurrentMinusTime());
+
+  const time = tempTime.getMinutes() > 40 ? new Date(date) : new Date(tempTime);
+
+  // const targetTime = String(
+  //   time.getMinutes() > 40 ? time.getHours() : time.getHours() - 1
+  // ).padStart(2, "0");
+
+  const targetTime = String(time.getHours()).padStart(2, "0");
+
+  // const targetDate =
+  //   Number(targetTime) > -1
+  //     ? time.getFullYear() +
+  //       String(Number(time.getMonth() + 1)).padStart(2, "0") +
+  //       String(Number(time.getDate())).padStart(2, "0")
+  //     : time.getFullYear() +
+  //       String(Number(time.getMonth() + 1)).padStart(2, "0") +
+  //       String(Number(time.getDate() - 1)).padStart(2, "0");
 
   const targetDate =
-    Number(targetTime) > -1
-      ? time.getFullYear() +
-        String(Number(time.getMonth() + 1)).padStart(2, "0") +
-        String(Number(time.getDate())).padStart(2, "0")
-      : time.getFullYear() +
-        String(Number(time.getMonth() + 1)).padStart(2, "0") +
-        time.getDate() -
-        1;
+    time.getFullYear() +
+    String(Number(time.getMonth() + 1)).padStart(2, "0") +
+    String(Number(time.getDate())).padStart(2, "0");
 
+  console.log(targetDate, targetTime);
   return [targetDate, targetTime];
 };
